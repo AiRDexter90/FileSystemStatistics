9c97ef33ee29bffdf1a8aa64b85701eb647a6b66
AiRDexter90 <piotrmarkowicz90@gmail.com>
2022-01-12 18:59:18 +0100

    Add files via upload
    Add comments

--- main.cpp
+++ main.cpp
@@ -12,13 +12,8 @@ using namespace std;
 
 int main()
 {
-    fs::path aPath{ "C:\\Users\\Piotr\\source\\repos\\Filesystem\\Text.txt" };
 
-    cout << "Parent path: " << aPath.parent_path() << endl;
-    cout << "Filename: " << aPath.filename() << endl;
-    cout << "Extension: " << aPath.extension() << endl;
-    //cout << ios::boolalpha << aPath.empty() << endl;
-
+    //there are created some directories and a few files for test iterating over given directory-------------------------------------------
     const fs::path files{ "C:\\Users\\Piotr\\Desktop\\PROGRAMOWANIE\\C++\\FileSystem" };
     std::filesystem::create_directories(files / "dir1" / "dir2");
     std::filesystem::create_directories(files / "dir11" / "dir12"/"dir13");
@@ -26,24 +21,21 @@ int main()
     std::ofstream{ files / "file2.txt" };
     std::ofstream{ files / "dir11" / "file11.txt" };
 
-    std::cout << "directory_iterator:\n";
-    for (auto const& dir_entry : std::filesystem::directory_iterator{ files })
-        cout << dir_entry.path() << '\n';
-
+    //recursive iterating over directory---------------------------------------------------------------------------------------------
     std::cout << "recursive_directory_iterator:\n";
     for (auto const& dir_entry : std::filesystem::recursive_directory_iterator{ files })
     {
-        fs::path subpath(dir_entry.path()); //sciezki podkatalogow lub plikow
-        std::cout << subpath << '\n';           //wypisz sciezke podkatalogu lub pliku
-        fs::file_status subpath_stat = fs::status(subpath);
-        if(fs::is_directory(subpath_stat)) std::cout <<"Katalog"<< '\n';
-        else 
+        fs::path subpath(dir_entry.path());                                 
+        std::cout << subpath << '\n';                               //print nodes names (directories and files)        
+        fs::file_status subpath_stat = fs::status(subpath);         //status let me know, if node is file or directory
+        if(fs::is_directory(subpath_stat)) std::cout <<"Directory"<< '\n';    //print info node is directory
+        else                                                                  //print info node is file and increment file counter
         {
-            std::cout << "Plik" << '\n'; counter++;
+            std::cout << "File" << '\n'; counter++;
         }
     } 
-    cout << "Jest " << counter << " plikow"  <<'\n';
-    std::filesystem::remove_all(files);
+    cout << "There are " << counter << " files"  <<'\n';                    //print the result
+    std::filesystem::remove_all(files);                                     //remove directory created for test
 
 	return 0;
 }
