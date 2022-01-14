3e8e34aab4072a2f2514a9e8ab09b9c7a63a5a3c
AiRDexter90 <piotrmarkowicz90@gmail.com>
2022-01-14 21:23:14 +0100

    Add files via upload
    Tidy up on code

--- main.cpp
+++ main.cpp
@@ -7,113 +7,125 @@
 #include <chrono>
 #include "thread_pool.h"
 
-using namespace std;
-typedef unsigned int uint;
-typedef unsigned long int ulint;
-namespace fs = std::filesystem;
-
-
 #define NR_THREADS 4
 #define PATH_1 "C:\\Users\\Piotr\\Desktop\\PROGRAMOWANIE\\C++\\FileSystem"
 #define PATH_2 "C:\\Users\\Piotr\\Desktop\\PROGRAMOWANIE\\C++\\OdczytZapisPlikow"
+#define PATH_3 "C:\\Users\\Piotr\\Desktop\\PROGRAMOWANIE\\C++\\BibliotekaGraficznaAllegro\\allegro"
+#define PATH_4 "C:\\Users\\Piotr\\Desktop\\PROGRAMOWANIE\\C++\\OdczytZapisPlikow\\New2"
 
-uint counter = 0;
 
-//fun will be used in thread
-void inspect_file(const fs::path & file)
+
+using namespace std;
+namespace fs = std::filesystem;
+
+typedef unsigned int uint;
+typedef unsigned long int ulint;
+typedef unsigned short int usint;
+
+//string s_parent_path;
+//*************************************GLOBAL VARIABLES*******************************************
+const usint max_nr_of_threads = thread::hardware_concurrency(); //number of available threads to use
+
+ulint count_directories;
+ulint count_files;
+ulint count_lines;
+ulint count_full_lines;
+ulint count_empty_lines;
+ulint count_words;
+ulint count_letters;
+
+mutex C;
+
+//***************************************FUNCTIONS************************************************
+
+//-----------------------------------------------------------------------------------------------
+
+
+//--------------------------------------------------------------------------------------------------------------------------
+//MAIN FUNCTION-------------------------------------------------------------------------------------------------------------
+void analyze_path(const fs::path& parent,usint requested_nr_of_threads)
 {
-    string pathname = file.string();
-    fstream  reader;
-    string buffer;									//a kind of container for txt lines
-    uint full_lines = 0, empty_lines = 0;
-
-    reader.open(pathname);
-
-        /*get consecutive lines of file count, and analyze them in the mean of words and letters number*/
-        while (!reader.eof())
-        {
-            buffer.clear();
-            getline(reader, buffer, '\n');
-            if (!buffer.empty()) full_lines++;
-            else empty_lines++;
-        }
-        cout << "Full line: " << full_lines << endl;
-        cout << "Empty line: " << empty_lines << endl;
-}
-
-
-//structure represent directory and its content
-struct dir
-{
-    fs::path raw_path;              //path to directory
-    string path;                    //path as the string
-    vector <dir> subdirs;           //vector of direct subdirectory
-    uint total_files;               //total number of files
-    uint total_lines;               //total number of lines
-    ulint total_words;               //total number of words
-    ulint total_letters;             //total number of letters
-    //counting totals - sum over vector
-};
-
-//main function, that will analyze the given path--------------------------------------
-void analyze_path(const fs::path& parent)
-{
-    thread_pool tp(NR_THREADS);
-    //tell which path to analyze
+    //LIMITATION OF THREADS
+    usint used_nr_of_threads = max_nr_of_threads;          
+    if (requested_nr_of_threads < max_nr_of_threads && !requested_nr_of_threads)
+    {
+        used_nr_of_threads = requested_nr_of_threads; //limit as user requested
+        cout << "Used number of threads: " << used_nr_of_threads;
+    }
+    else
+    {
+        cout << "Used number of threads limited to max: " << used_nr_of_threads;
+    }
+                                        
+    //CONSTRUCTION OF THREAD POOL
+    thread_pool tp(used_nr_of_threads); 
+    
+    //tell the analyzed path
     {
         string s_parent_path = parent.string();
-        cout << "Wyniki analizy katalogu: " << s_parent_path << endl;
+        cout << "Results for path: " << s_parent_path << endl;
     }
 
+    //SET OF COUNTERS
+    
+
+    //FIND COUNTABLE (LINES,WORDS,LETTERS) FILES AND COUNT OVERALL NUMBER OF FILES - USED RECURSIVE ITERATOR
     for (auto const& dir_entry : std::filesystem::recursive_directory_iterator{ parent })
     {
+        //CHECK IF NODE IS DIRECTORY OR FILE
         fs::path subpath(dir_entry.path());
-        std::cout << subpath << '\n';                               //print nodes names (directories and files)        
-        fs::file_status subpath_stat = fs::status(subpath);         //status let me know, if node is file or directory
-        if (fs::is_directory(subpath_stat)) std::cout << "Directory" << '\n';    //print info node is directory
-        else                                                                  //print info node is file and increment file counter
+        fs::file_status subpath_stat = fs::status(subpath);                     
+        if (fs::is_directory(subpath_stat)) count_directories++;     //count up directories  
+        else                                                                  
         {
-            std::cout << "File" << '\n'; counter++;
-            //inspect_file(subpath);
-            
+            count_files++;                                            //count up files
+ 
+            //ENQUEUE TASK TO ANALYZE A FILE 
             tp.enqueue([subpath] {
-                int max = 1000000000;
-                for (int i = 0; i < max; i++)
-                {
-                    //empty
-                }
+
+                //ANALYZE OF A FILE
                 string pathname = subpath.string();
                 fstream  reader;
-                string buffer;									//a kind of container for txt lines
-                uint full_lines = 0, empty_lines = 0;
+                string buffer;			//to analyze line by line
 
+                //READ LINES AND COUNT
                 reader.open(pathname);
-
-                /*get consecutive lines of file count, and analyze them in the mean of words and letters number*/
                 while (!reader.eof())
                 {
                     buffer.clear();
                     getline(reader, buffer, '\n');
-                    if (!buffer.empty()) full_lines++;
-                    else empty_lines++;
+                    C.lock();
+                    if (!buffer.empty()) count_full_lines++;
+                    else count_empty_lines++;
+                    C.unlock();
                 }
-                cout << "Full line: " << full_lines << endl;
-                cout << "Empty line: " << empty_lines << endl; });
+            });
         }
     }
-    cout << "There are " << counter << " files" << '\n';                    //print the result
-
+    
 }
 
 int main()
 {
-    fs::path files{PATH_2};
-    auto start_4 = chrono::steady_clock::now();
-    analyze_path(files);
-    auto stop_4 = chrono::steady_clock::now();
+   // cout << "Enter the path of analyzed directory: ";
+    //cin >> s_parent_path;
+    
+
+    fs::path files{PATH_4};
+    auto start = chrono::steady_clock::now();
+    analyze_path(files,4);
+    auto stop = chrono::steady_clock::now();
+
+    count_lines = count_full_lines + count_empty_lines;
+    cout << "There are: " << endl;
+    cout << count_files << " files" << '\n';                    //print the result
+    cout << count_directories << " directories" << '\n';
+    cout << count_lines << " lines" << '\n';
+    cout << count_full_lines << "full lines" << '\n';
+    cout << count_empty_lines << "empty lines" << '\n';
 
     cout << "Number of used threads: " << NR_THREADS << endl;
-    chrono::duration<double> elapsed_time_4 = stop_4 - start_4;
-    cout << "Required time: " << elapsed_time_4.count() << endl;
+    chrono::duration<double> elapsed_time = stop - start;
+    cout << "Required time: " << elapsed_time.count() << endl;
     return 0;
 }
